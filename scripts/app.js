f18b139c6df640c4078859e518fd30087afb61c5
Jisang Hong <jisang1024@gmail.com>
2023-01-27 23:12:25 +0900

    JavaScript to game logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,11 @@
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   { name: "", symbol: "X" },
@@ -9,10 +16,15 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-error");
+//const gameAreaElement = document.getElementById("active-game");
+const gameBoardElement = document.getElementById("game-board");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
+const gameFieldElements = document.querySelectorAll("#game-board li");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -21,3 +33,10 @@ cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
 
 formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
+
+//for (const gameFieldElement of gameFieldElements) {
+//  gameFieldElement.addEventListener("click", selectGameField);
+//}
+gameBoardElement.addEventListener("click", selectGameField);
